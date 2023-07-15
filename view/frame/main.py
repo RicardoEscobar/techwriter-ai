394045bcde719eead50518fc7dca749c03a0bed2
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-15 15:29:58 -0600

    Add the first input for system at: view\frame\main.py

--- view/frame/main.py
+++ view/frame/main.py
@@ -24,7 +24,7 @@ class MainFrame(ttk.Frame):
         # Add a label to the frame
         self.system_label = ttk.Label(self, text="System: ")
         # Add an entry field to the frame
-        self.system_text = tk.Text(self, height=10, wrap="word")
+        self.system_text = tk.Text(self, height=5, wrap="word")
         # Add a scrollbar to the entry field
         self.system_scroll = ttk.Scrollbar(self, orient="vertical", command=self.system_text.yview)
         # Add a button to the frame
@@ -34,7 +34,7 @@ class MainFrame(ttk.Frame):
         self.system_label.grid(row=0, column=0, sticky="n")
         self.system_text.grid(row=0, column=1, sticky="nsew")
         self.system_scroll.grid(row=0, column=1, sticky="nse")
-        self.prompt_button.grid(row=0, column=2, sticky="nsew")
+        self.prompt_button.grid(row=0, column=2, sticky="new")
 
         # make the entry field expand to fill the frame
         self.system_text.grid_rowconfigure(0, weight=1)
@@ -43,7 +43,6 @@ class MainFrame(ttk.Frame):
 
         # make the frame expand to fill the root
         self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
 
 if __name__ == "__main__":
     root = tk.Tk()
