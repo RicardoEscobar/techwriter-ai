495d4e28d8fbe80e214fa357fbcbe5041f0a9e62
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-15 20:22:21 -0600

    Refactor to reuse input frames for user, system and assistant roles: view\frame\main.py

--- view/frame/main.py
+++ view/frame/main.py
@@ -2,14 +2,15 @@
 import tkinter as tk
 from tkinter import ttk
 
+from view.frame.input import InputFrame
+
 
 class MainFrame(ttk.Frame):
     """This tkinter frame is the main frame of the application."""
     
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         """Initialize the MainFrame."""
         super().__init__(parent)
-        self.controller = controller
         
         # Make the main frame expand to fill the root
         self.grid(row=0, column=0, sticky="nsew")
@@ -19,34 +20,21 @@ class MainFrame(ttk.Frame):
 
     def create_widgets(self):
         """Create the widgets for the MainFrame."""
-        self.master.title("Techwriter's AI")
+        self.master.title("TechWriter AI")
         self.master.geometry("800x600")
-        # Add a label to the frame
-        self.system_label = ttk.Label(self, text="System: ")
-        # Add an entry field to the frame
-        self.system_text = tk.Text(self, height=5, wrap="word")
-        # Add a scrollbar to the entry field
-        self.system_scroll = ttk.Scrollbar(self, orient="vertical", command=self.system_text.yview)
-        # Add a button to the frame
-        self.prompt_button = ttk.Button(self, text="Prompt", command=self.controller)
 
-        # Add the widgets to the frame
-        self.system_label.grid(row=0, column=0, sticky="n")
-        self.system_text.grid(row=0, column=1, sticky="nsew")
-        self.system_scroll.grid(row=0, column=1, sticky="nse")
-        self.prompt_button.grid(row=0, column=2, sticky="new")
+        # Add an input frame for the system, user, assistant roles
+        self.system_input_frame = InputFrame(self, text="System")
+        self.user_input_frame = InputFrame(self, text="User")
+        self.assistant_input_frame = InputFrame(self, text="Assistant")
 
-        # make the entry field expand to fill the frame
-        self.system_text.grid_rowconfigure(0, weight=1)
-        self.system_text.grid_columnconfigure(0, weight=1)
-        self.system_text.configure(yscrollcommand=self.system_scroll.set)
-
-        # make the frame expand to fill the root
-        self.columnconfigure(1, weight=1)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    controller = lambda: print("Hello World!")
-    main_frame = MainFrame(root, controller)
-    main_frame.pack(fill="both", expand=True)    
-    root.mainloop()
+        # Make the system, user and assistant input frames expand to fill the root horizontally
+        self.system_input_frame.grid(row=0, column=0, sticky="ew")
+        self.system_input_frame.columnconfigure(0, weight=1)
+        self.user_input_frame.grid(row=1, column=0, sticky="ew")
+        self.user_input_frame.columnconfigure(0, weight=1)
+        self.assistant_input_frame.grid(row=2, column=0, sticky="ew")
+        self.assistant_input_frame.columnconfigure(0, weight=1)
+        
+        # Make the main frame expand to fill the root horizontally
+        self.columnconfigure(0, weight=1)
